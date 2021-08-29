9679630ffa311f87349efa9321b0cd22aee10a9d
ifjso <ojsboy@hotmail.com>
2021-08-30 02:20:34 +0900

    feat: heartbeat 추가

--- src/main/java/com/js/websocketdemo/WebSocketConfig.java
+++ src/main/java/com/js/websocketdemo/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.js.websocketdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -12,8 +15,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic")
+            .setTaskScheduler(scheduler())
+            .setHeartbeatValue(new long[] { 1000L, 1000L });
         config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Bean
+    public TaskScheduler scheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("Scheduler-Thread-");
+        scheduler.initialize();
+        return scheduler;
     }
 
     @Override
